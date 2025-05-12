eeddc7ad296d1343d89f3a489fc1a26c1dcdbaef
Frank Arteaga <farteaga8912@users.noreply.github.com>
2025-05-11 20:39:33 -0400

    test

--- test/non_empty_board_configuration_test.exs
+++ test/non_empty_board_configuration_test.exs
@@ -2,7 +2,7 @@ defmodule NonEmptyBoardConfigurationTest do
   use ExUnit.Case
 
   alias Battleship.BoardConfiguration
-
+   # ttest
   test "create_empty_board" do
     assert BoardConfiguration.create_empty_board(1) == %BoardConfiguration{size: 1, matrix: %{
       {1, 1} => :empty
