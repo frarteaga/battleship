29f9e5c84e85276678971f394aba9f3f877d5a28
Frank Arteaga <farteaga8912@users.noreply.github.com>
2025-05-11 20:24:18 -0400

    red test

---
+++ test/non_empty_board_configuration_test.exs
@@ -0,0 +1,24 @@
+defmodule NonEmptyBoardConfigurationTest do
+  use ExUnit.Case
+
+  alias Battleship.BoardConfiguration
+
+  test "create_empty_board" do
+    assert BoardConfiguration.create_empty_board(1) == %BoardConfiguration{size: 1, matrix: %{
+      {1, 1} => :empty
+    }}
+  end
+
+  def non_empty_board_1x1(%BoardConfiguration{size: 1, matrix: %{
+    {1, 1} => :ship
+  }}) do
+    expected_string = """
+        A
+      +---+
+    1 | @ |
+      +---+
+    """
+    expected_string
+  end
+
+end
