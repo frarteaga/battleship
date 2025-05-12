a4714b45a75169a117331a9d863ef983f1102625
Frank Arteaga <farteaga8912@users.noreply.github.com>
2025-05-11 21:02:50 -0400

    Enhance tests for adding ships to the board in NonEmptyBoardConfigurationTest
    - Added tests for adding ships of size 2 in both right and down directions.
    - Introduced expected board representations for 1x1 and 2x2 configurations.
    - Updated existing tests to utilize new board representations for improved clarity.

--- test/non_empty_board_configuration_test.exs
+++ test/non_empty_board_configuration_test.exs
@@ -3,29 +3,83 @@ defmodule NonEmptyBoardConfigurationTest do
 
   alias Battleship.BoardConfiguration
 
-  test "create_empty_board and add a ship of size 1" do
+  @non_empty_board_1x1 %BoardConfiguration{size: 1, matrix: %{
+    {1, 1} => :ship
+  }}
+
+  @non_empty_board_1x1_repr """
+      A
+    +---+
+  1 | @ |
+    +---+
+  """
+
+  @non_empty_board_2x2_repr_right %BoardConfiguration{size: 2, matrix: %{
+    {1, 1} => :ship,
+    {1, 2} => :ship,
+    {2, 1} => :empty,
+    {2, 2} => :empty
+  }}
+
+  @non_empty_board_2x2_repr_right_repr """
+      A   B
+    +---+---+
+  1 | @ | @ |
+    +---+---+
+  2 | ~ | ~ |
+    +---+---+
+  """
+
+  @non_empty_board_2x2_repr_down %BoardConfiguration{size: 2, matrix: %{
+    {1, 1} => :ship,
+    {2, 1} => :ship,
+    {1, 2} => :empty,
+    {2, 2} => :empty
+  }}
+
+  @non_empty_board_2x2_repr_down_repr """
+      A   B
+    +---+---+
+  1 | @ | ~ |
+    +---+---+
+  2 | @ | ~ |
+    +---+---+
+  """
+
+  test "create_empty_board and add a ship of size 1 to the right" do
     board = BoardConfiguration.create_empty_board(1)
     position = {1, 1}
     direction = :right # or :down
     size = 1
     board = BoardConfiguration.add_ship(board, position, direction, size)
-    expected_board = %BoardConfiguration{size: 1, matrix: %{
-      {1, 1} => :ship
-    }}
-    assert board == expected_board
+    assert board == @non_empty_board_1x1
   end
 
-  def non_empty_board_1x1(%BoardConfiguration{size: 1, matrix: %{
-    {1, 1} => :ship
-  }}) do
-    expected_string = """
-        A
-      +---+
-    1 | @ |
-      +---+
-    """
-    expected_string
+  test "create_empty_board and add a ship of size 1 to the down" do
+    board = BoardConfiguration.create_empty_board(1)
+    position = {1, 1}
+    direction = :down
+    size = 1
+    board = BoardConfiguration.add_ship(board, position, direction, size)
+    assert board == @non_empty_board_1x1
   end
 
+  test "create_empty_board and add a ship of size 2 to the right" do
+    board = BoardConfiguration.create_empty_board(2)
+    position = {1, 1}
+    direction = :right
+    size = 2
+    board = BoardConfiguration.add_ship(board, position, direction, size)
+    assert board == @non_empty_board_2x2_repr_right
+  end
+
+  test "create_empty_board and add a ship of size 2 to the down" do
+    board = BoardConfiguration.create_empty_board(2)
+    position = {1, 1}
+    direction = :down
+    size = 2
+    board = BoardConfiguration.add_ship(board, position, direction, size)
+    assert board == @non_empty_board_2x2_repr_down
+  end
 
 end
