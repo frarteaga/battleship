46f2c9c7215dfec97e615b610b3f8f7e913a8df0
Frank Arteaga <farteaga8912@users.noreply.github.com>
2025-05-14 19:02:34 -0400

    Implement create_ship/1 with validation and error handling
    - Introduced a non-bang version of the ship creation function that validates positions and handles errors gracefully.
    - Added a new validate_position?/1 function to check if the given position is valid.
    - Updated tests to cover the new functionality, including successful ship creation and error scenarios for invalid positions.

--- lib/ship.ex
+++ lib/ship.ex
@@ -10,11 +10,29 @@ defmodule Battleship.Ship do
     hits: list(Battleship.BoardConfiguration.position())
   }
 
-  def create_ship(size, position, direction) do
+  def create_ship!(size, position, direction) do
     positions = case direction do
       :right -> Enum.map(0..(size - 1), fn i -> {position |> elem(0), (position |> elem(1)) + i} end)
       :down -> Enum.map(0..(size - 1), fn i -> {(position |> elem(0)) + i, position |> elem(1)} end)
     end
     %__MODULE__{positions: positions, hits: []}
+  end
+
+  def create_ship(size, position, direction) do
+    if validate_position?(position) do
+      try do
+        {:ok, create_ship!(size, position, direction)}
+      rescue
+        error -> {:error, error}
+      end
+    else
+      {:error, "Invalid position"}
+    end
+  end
+
+  def validate_position?(position) do
+    x = elem(position, 0)
+    y = elem(position, 1)
+    x >= 1 && y >= 1
   end
 end
--- test/ship_test.exs
+++ test/ship_test.exs
@@ -2,23 +2,54 @@ defmodule Battleship.ShipTest do
   use ExUnit.Case
   alias Battleship.Ship
 
-  test "create_ship/1 creates a ship of size 2 at a given position, to the right, at the beggining the ship is not hit" do
+  test "create_ship!/1 creates a ship of size 2 at a given position, to the right, at the beggining the ship is not hit" do
     size = 2
     position = {1, 1}
     direction = :right
-    ship = Ship.create_ship(size, position, direction)
+    ship = Ship.create_ship!(size, position, direction)
     assert length(ship.positions) == size
     assert ship.positions == [{1, 1}, {1, 2}]
     assert ship.hits == []
   end
 
-  test "create_ship/1 creates a ship of size 2 at a given position, to the down, at the beggining the ship is not hit" do
+  test "create_ship!/1 creates a ship of size 2 at a given position, to the down, at the beggining the ship is not hit" do
     size = 2
     position = {1, 1}
     direction = :down
-    ship = Ship.create_ship(size, position, direction)
+    ship = Ship.create_ship!(size, position, direction)
     assert length(ship.positions) == size
     assert ship.positions == [{1, 1}, {2, 1}]
     assert ship.hits == []
+  end
+
+  test "create_ship/1 no bang version, returns {:ok, ship} or {:error, reason}" do
+    size = 2
+    position = {1, 1}
+    direction = :right
+    {:ok, ship} = Ship.create_ship(size, position, direction)
+    assert length(ship.positions) == size
+  end
+
+  test "prevent ship creation outside of the board, position values should be >= {1, 1}" do
+    size = 2
+    position = {0, 1}
+    direction = :right
+    {:error, reason} = Ship.create_ship(size, position, direction)
+    assert reason == "Invalid position"
+  end
+
+  test "validate_position/1 returns false if the x position is invalid" do
+    position = {0, 1}
+    assert Ship.validate_position?(position) == false
+  end
+
+  test "validate_position/1 returns false if the y position is invalid" do
+    position = {1, 0}
+    assert Ship.validate_position?(position) == false
+  end
+
+  test "validate_position/1 returns true if the position is valid" do
+    position = {1, 1}
+    assert Ship.validate_position?(position) == true
   end
 end
